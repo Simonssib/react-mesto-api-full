186c1b0c436da4ce27e8c195c0b35b73af5d5039
Simon Alex <alekseev-simon@mail.ru>
2022-10-14 21:52:01 +0300

    correct cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,7 +12,7 @@ const cors = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const DEFAULT_ALLOWED_METHODS = ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
